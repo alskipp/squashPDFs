ac5d69943f79c4f7992a1f7d89fe6ecfc8b9cb59
Al Skipp <al@Als-MacBook-Air.broadband>
2020-05-05 16:32:32 +0100

    Matches `pdf` file extension ignoring case

--- make_ebook_pdf.py
+++ make_ebook_pdf.py
@@ -1,8 +1,9 @@
-import os.path
-import glob
-import tempfile
-import subprocess
 import argparse
+import fnmatch
+import glob
+import os
+import subprocess
+import tempfile
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
@@ -13,20 +14,24 @@ def merge_pages(path_1, path_2):
     master_page = page_1_reader.getPage(0)
     overlay_page = page_2_reader.getPage(0)
 
-    (tx, ty) = translateToCenter(master_page.mediaBox, overlay_page.mediaBox)
+    return overlay_text(master_page, overlay_page)
+
+
+def overlay_text(master_page, overlay_page):
+    (tx, ty) = translate_to_center(master_page.mediaBox, overlay_page.mediaBox)
     master_page.mergeTranslatedPage(overlay_page, tx, ty)
 
     return master_page
 
 
-def translateToCenter(rect1, rect2):
+def translate_to_center(rect1, rect2):
     (x1, y1) = rect1.upperRight
     (x2, y2) = rect2.upperRight
     return ((x1 - x2) / 2, (y1 - y2) / 2)
 
 
-def compress_pdf(input, output):
-    gs = f"gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile={output} {input}"
+def compress_pdf(input_file, output_file):
+    gs = f"gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile={output_file} {input_file}"
     subprocess.call(gs, shell=True)
 
 
@@ -35,22 +40,26 @@ def process_files(output, pages, text):
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         for page_path, text_path in zip(pages, text):
-            compressed_file = os.path.join(
+            compressed_file_path = os.path.join(
                 tmpdirname, os.path.basename(page_path))
 
             print(
                 f"processing: {os.path.basename(page_path)} & {os.path.basename(text_path)}")
 
-            compress_pdf(page_path, compressed_file)
+            compress_pdf(page_path, compressed_file_path)
 
-            pdf_writer.addPage(merge_pages(compressed_file, text_path))
+            pdf_writer.addPage(merge_pages(compressed_file_path, text_path))
 
     with open(output, 'wb') as fh:
         print(f"writing output file: {output}")
         pdf_writer.write(fh)
 
 
-if __name__ == '__main__':
+def pdf_files(dir):
+    return sorted(glob.glob(dir+'/*.[pP][dD][fF]'))
+
+
+def main():
     parser = argparse.ArgumentParser(
         description='Combine image and text PDFs into low-res PDF')
     parser.add_argument('image_dir', type=str,
@@ -62,9 +71,13 @@ if __name__ == '__main__':
 
     args = vars(parser.parse_args())
 
-    pages = sorted(glob.glob(args['image_dir']+'/*.pdf'))
-    text = sorted(glob.glob(args['text_dir']+'/*.pdf'))
+    pages = pdf_files(args['image_dir'])
+    text = pdf_files(args['text_dir'])
     process_files(args['output_file'], pages, text)
+
+
+if __name__ == '__main__':
+    main()
 
 # Example Terminal useage:
 # To get help:
